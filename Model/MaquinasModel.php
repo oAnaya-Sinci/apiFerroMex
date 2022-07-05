8d51aa4708a929ff9a74c3c316b2f30253397cc3
Carlos Anaya <a_naya616@hotmail.com>
2022-07-05 17:40:22 -0500

    First commit to upload the data of the api project for FerroMex

---
+++ Model/MaquinasModel.php
@@ -0,0 +1,40 @@
+<?php
+require_once PROJECT_ROOT_PATH . "/Model/Database.php";
+ 
+class MaquinasModel extends Database
+{
+    public function getDataGPS()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+
+    public function getAcumuladoDiesel()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+
+    public function getAcumuladoKilometers()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+    
+    public function getAVGDiesel()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+
+    public function getAVGKilometers()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+    
+    public function getRegistroNotificacion()
+    {
+        return $this->select("SELECT * FROM registrosmaquinarias");
+    }
+    
+    public function setRegistroNotificacion()
+    {
+        return $this->select("INSERT INTO alarmsLogs() VALUES()");
+    }
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . "/inc/bootstrap.php";
+ 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+ 
+if ((isset($uri[3]) && $uri[3] != 'maquinas') || !isset($uri[4])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+require PROJECT_ROOT_PATH . "/Controller/Api/MaquinasController.php";
+
+$objFeedController = new MaquinasController();
+$strMethodName = $uri[4];
+$objFeedController->{$strMethodName}();
+?>

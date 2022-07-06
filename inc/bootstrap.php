167bfcb4beeef0a2accff9c9c5106dee43e3e4e6
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 11:52:17 -0500

    Changiues to test the API funcionality v2.5

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,19 +1,17 @@
 <?php
 // define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-// define("PROJECT_ROOT_PATH",  "http://192.168.0.101/api");
- 
-// die( var_dump( PROJECT_ROOT_PATH ) );
+define("PROJECT_ROOT_PATH",  $_SERVER['DOCUMENT_ROOT']."/api");
 
 // include main configuration file
-// require_once PROJECT_ROOT_PATH . "/inc/config.php";
-require_once "inc/config.php";
+require_once PROJECT_ROOT_PATH . "/inc/config.php";
+// require_once "inc/config.php";
  
 // include the base controller file
-// require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
-require_once "Controller/Api/BaseController.php";
+require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+// require_once "Controller/Api/BaseController.php";
  
 // include the use model file
 // require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
 
-// require_once PROJECT_ROOT_PATH . "/Model/MaquinasModel.php";
-require_once "Model/MaquinasModel.php";
+require_once PROJECT_ROOT_PATH . "/Model/MaquinasModel.php";
+// require_once "Model/MaquinasModel.php";
--- info.php
+++ info.php
@@ -1,14 +1,9 @@
 <?php
 
-// require "inc/bootstrap.php";
-
-
-// require_once "inc/config.php";
-// require_once "Controller/Api/BaseController.php";
-// require_once "Model/MaquinasModel.php";
+require $_SERVER['DOCUMENT_ROOT']."/api/inc/bootstrap.php";
 
 // die( var_dump(__DIR__) );
-die( var_dump($_SERVER['DOCUMENT_ROOT']) );
+// die( var_dump($_SERVER['DOCUMENT_ROOT']) );
 
 // Print the informatrion of PHP
 echo phpinfo();

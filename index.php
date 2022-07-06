2fa39af02afa61f4060668325f0e4fa7d235c20c
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 12:25:43 -0500

    Changiues to test the API funcionality v2.7.9

--- index.php
+++ index.php
@@ -12,6 +12,11 @@ if ((isset($uri[3]) && $uri[3] != 'maquinas') || !isset($uri[4])) {
 require PROJECT_ROOT_PATH . "/Controller/api/MaquinasController.php";
 
 $objFeedController = new MaquinasController();
-$strMethodName = $uri[4];
+$strMethodName = $uri[4]."()";
+
 die( var_dump( $strMethodName ) );
-$objFeedController->{$strMethodName}();
+
+// $objFeedController->{$strMethodName}();
+$objFeedController->$strMethodName;
+
+die( var_dump( $uri ) );

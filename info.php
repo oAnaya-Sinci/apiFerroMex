0875835cf9eb4ddb9fddeef12869ce04424bfa71
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 11:50:00 -0500

    Changiues to test the API funcionality v2.3

--- info.php
+++ info.php
@@ -1,6 +1,14 @@
 <?php
 
-require "inc/bootstrap.php";
+// require "inc/bootstrap.php";
+
+
+require_once "inc/config.php";
+require_once "Controller/Api/BaseController.php";
+require_once "Model/MaquinasModel.php";
+
+// die( var_dump(__DIR__) );
+die( var_dump($_SERVER['DOCUMENT_ROOT']) );
 
 // Print the informatrion of PHP
 echo phpinfo();

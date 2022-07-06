c2fbc37d01be1bced916b192677e2aa0a81a5daf
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 11:50:25 -0500

    Changiues to test the API funcionality v2.4

--- info.php
+++ info.php
@@ -3,9 +3,9 @@
 // require "inc/bootstrap.php";
 
 
-require_once "inc/config.php";
-require_once "Controller/Api/BaseController.php";
-require_once "Model/MaquinasModel.php";
+// require_once "inc/config.php";
+// require_once "Controller/Api/BaseController.php";
+// require_once "Model/MaquinasModel.php";
 
 // die( var_dump(__DIR__) );
 die( var_dump($_SERVER['DOCUMENT_ROOT']) );

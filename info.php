9c1905c2e63e1a48cbd34c922b806994f97b0e9f
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 10:39:11 -0500

    Changiues to test the API funcionality v1.4

--- info.php
+++ info.php
@@ -1,9 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
-
-die( var_dump( $uri ) );
+require "inc/bootstrap.php";
 
 // Print the informatrion of PHP
 echo phpinfo();

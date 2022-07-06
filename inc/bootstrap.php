993b48ec9131cb35afb49118e24e0f8902103997
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 10:51:39 -0500

    Changiues to test the API funcionality v1.9

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 // define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-define("PROJECT_ROOT_PATH",  "http://192.168.0.101/");
+define("PROJECT_ROOT_PATH",  "http://192.168.0.101/api/");
  
-die( var_dump( PROJECT_ROOT_PATH ) );
+// die( var_dump( PROJECT_ROOT_PATH ) );
 
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/inc/config.php";

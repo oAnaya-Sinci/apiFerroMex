773fe639813d04f17446065eee8c32f0635f57ba
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 11:57:47 -0500

    Changiues to test the API funcionality v2.6.2

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -2,12 +2,12 @@
 // define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 define("PROJECT_ROOT_PATH",  $_SERVER['DOCUMENT_ROOT']."api");
 
-die( var_dump( PROJECT_ROOT_PATH ) );
-
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/inc/config.php";
 // require_once "inc/config.php";
  
+die( var_dump( PROJECT_ROOT_PATH ) );
+
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
 // require_once "Controller/Api/BaseController.php";

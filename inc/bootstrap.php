c18b0dbc09a57ed18c1b69d3c88f32d76c11f387
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 10:24:42 -0500

    Changiues to test the API funcionality v1.1

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+// define("PROJECT_ROOT_PATH",  "..");
  
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/inc/config.php";
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require __DIR__ . "/inc/bootstrap.php";
- 
+// require __DIR__ . "/inc/bootstrap.php";
+
+require "inc/bootstrap.php";
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
  

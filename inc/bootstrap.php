93417d8b18d5a7bbc27da049da03c56c3dfce4c5
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 10:33:03 -0500

    Changiues to test the API funcionality v1.3

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -12,4 +12,3 @@ require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
 // require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
 
 require_once PROJECT_ROOT_PATH . "/Model/MaquinasModel.php";
-?>
--- info.php
+++ info.php
@@ -1,3 +1,9 @@
 <?php
 
-  echo phpinfo();
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+die( var_dump( $uri ) );
+
+// Print the informatrion of PHP
+echo phpinfo();

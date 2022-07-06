18f3749be2270bfd0f27dcddd39889fabd813010
Carlos Anaya <a_naya616@hotmail.com>
2022-07-06 11:38:52 -0500

    Changiues to test the API funcionality v2.1

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,16 +1,19 @@
 <?php
 // define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-define("PROJECT_ROOT_PATH",  "http://192.168.0.101/api/");
+// define("PROJECT_ROOT_PATH",  "http://192.168.0.101/api");
  
-die( var_dump( PROJECT_ROOT_PATH ) );
+// die( var_dump( PROJECT_ROOT_PATH ) );
 
 // include main configuration file
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
+// require_once PROJECT_ROOT_PATH . "/inc/config.php";
+require_once "inc/config.php";
  
 // include the base controller file
-require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+// require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+require_once "Controller/Api/BaseController.php";
  
 // include the use model file
 // require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
 
-require_once PROJECT_ROOT_PATH . "/Model/MaquinasModel.php";
+// require_once PROJECT_ROOT_PATH . "/Model/MaquinasModel.php";
+require_once "Model/MaquinasModel.php";

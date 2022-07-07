a0d8148abf751cc02be4c5a1cfbf99cc44b69e09
Carlos Anaya <a_naya616@hotmail.com>
2022-07-07 13:09:47 -0500

    Changues to test the API funcionality v3

--- Model/DataBase.php
+++ Model/DataBase.php
@@ -7,12 +7,12 @@ class DataBase
     {
         try {
             $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
-         
+                 
             if ( mysqli_connect_errno()) {
                 throw new Exception("Could not connect to database.");   
             }
         } catch (Exception $e) {
-            throw new Exception($e->getMessage());   
+            throw new Exception($e->getMessage());
         }           
     }
  
